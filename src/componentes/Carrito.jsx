1e23ffda2dac5a8c5572bf83c43824f42d17af46
sebastiangaribaldi <sebastian.garibaldi@outlook.com>
2025-10-16 09:33:51 -0300

    actualizo con privado en oficina 1

--- src/componentes/Carrito.jsx
+++ src/componentes/Carrito.jsx
@@ -1,27 +1,24 @@
 import Styles from './Carrito.module.css'
 
 const Carrito = ({ productosEnCarrito, productosEliminados }) => {
-    return (
-      <div className={Styles.productos}>
-        <h2>Carrito</h2>
-        {productosEnCarrito.map((producto, indice) => (
-          <table className={Styles.tabla}>
-            <tr>
-              <div className={Styles.prod} key={indice}>
-              <td>
-                <img src={producto.image} alt={producto.title} height={80} width={80} /> 
-              </td>
-              <td>
-                <h4 key={producto.indice}> {producto.title} </h4>
-                <h5>$ {producto.price}</h5>
-                <button onClick={() => productosEliminados(indice)}>Eliminar</button>
-              </td>
-              </div>
-            </tr>
-            </table>  
-        ))}
-      </div>
-    );
-  };
-  
-  export default Carrito; 
+  return (
+    <div className={Styles.carritoContainer}>
+      <h2>Lista de Comrpas</h2>
+      {productosEnCarrito.length === 0 ? (
+        <p>El carrito está vacío.</p>
+      ) : (
+        productosEnCarrito.map((producto, indice) => (
+          <div className={Styles.carritoItem} key={indice}>
+            <img className={Styles.itemImagen} src={producto.image} alt={producto.title} />
+            <div className={Styles.itemDetalles}>
+              <h4>{producto.title}</h4>
+              <h5>$ {producto.price}</h5>
+            </div>
+            <button className={Styles.botonEliminar} onClick={() => productosEliminados(indice)}>Eliminar</button>
+          </div>
+        )))}
+    </div>
+  );
+};
+
+export default Carrito;
