1477c69618548a937faa0398d5a64f077950aa23
sebastiangaribaldi <sebastian.garibaldi@outlook.com>
2025-10-16 09:55:20 -0300

    oficna 2 ok publico

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
     return(
         <header className="encabezado"  >
-                <h2> Bienvenidos Carrito SG </h2>
+                <h2> Bienvenidos Carrito SG - Publico -</h2>
             </header>
     );
 }
